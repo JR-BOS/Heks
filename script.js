b41a686cf33698bdfb900c8bf5629582d83c6584
Jesse-Bos <76525071+Jesse-Bos@users.noreply.github.com>
2021-03-12 15:12:35 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const home_content = document.querySelector(".home-content");
+const menu = document.querySelector(".navbarcontent");
+
+const headline = document.querySelector(".headline");
+const headline2 = document.querySelector(".headline2");
+const homebutton = document.querySelector(".homebutton");
+
+const timeLine = gsap.timeline();
+timeLine
+  .fromTo(
+    home_content,
+    1,
+    { height: "0%" },
+    { height: "80%", ease: Power2.easeInOut }
+  )
+
+  .fromTo(menu, 0.7, { opacity: 0 }, { opacity: 1 })
+  .fromTo(headline, 0.7, { opacity: 0, x: 0 }, { opacity: 1, x: 0 }, "-=0.7")
+  .fromTo(headline2, 0.7, { opacity: 0, x: 0 }, { opacity: 1, x: 0 }, "-=0.7")
+  .fromTo(homebutton, 0.7, { opacity: 0, x: 0 }, { opacity: 1, x: 0 }, "-=0.7");
