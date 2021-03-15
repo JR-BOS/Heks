606f493ab9852856fdbb4191bee6c375d1199111
Jesse-Bos <jesse_bos@live.com>
2021-03-15 21:59:16 +0100

    testje

--- script.js
+++ script.js
@@ -10,30 +10,24 @@ $(document).ready(function () {
       $(".navbar").removeClass("sticky");
     }
   });
-  // const home_content = document.querySelector(".home-content");
-  // const menu = document.querySelector(".navbarcontent");
+  const home_content = document.querySelector(".home-content");
+  const menu = document.querySelector(".navbarcontent");
 
-  // const headline = document.querySelector(".headline");
-  // const headline2 = document.querySelector(".headline2");
-  // const homebutton = document.querySelector(".homebutton");
+  const headline = document.querySelector(".headline");
+  const headline2 = document.querySelector(".headline2");
+  const homebutton = document.querySelector(".test");
 
-  // const timeLine = gsap.timeline();
-  // timeLine
-  //   .fromTo(
-  //     home_content,
-  //     0,
-  //     { height: "0%" },
-  //     { height: "80%", ease: Power2.easeInOut }
-  //   )
+  const timeLine = gsap.timeline();
+  timeLine
 
-  //   .fromTo(menu, 0.7, { opacity: 0 }, { opacity: 1 })
-  //   .fromTo(headline, 0.7, { opacity: 0, x: 0 }, { opacity: 1, x: 0 }, "-=0.7")
-  //   .fromTo(headline2, 0.7, { opacity: 0, x: 0 }, { opacity: 1, x: 0 }, "-=0.7")
-  //   .fromTo(
-  //     homebutton,
-  //     0.7,
-  //     { opacity: 0, x: 0 },
-  //     { opacity: 1, x: 0 },
-  //     "-=0.7"
-  //   );
+    .fromTo(menu, 0.7, { opacity: 0 }, { opacity: 1 })
+    .fromTo(headline, 0.7, { opacity: 0, x: 0 }, { opacity: 1, x: 0 }, "-=0.7")
+    .fromTo(headline2, 0.7, { opacity: 0, x: 0 }, { opacity: 1, x: 0 }, "-=0.7")
+    .fromTo(
+      homebutton,
+      0.7,
+      { opacity: 0, x: 0 },
+      { opacity: 1, x: 0 },
+      "-=0.7"
+    );
 });
