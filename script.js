d6c447350bf78aabb24c1a474ee1fa7f30ce888a
Jesse-Bos <jesse_bos@live.com>
2021-03-15 22:36:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 $(document).ready(function () {
-  $(".navbar-nav>li>a").on("click", function () {
-    $(".navbar-collapse").collapse("hide");
-  });
-
   $(window).scroll(function () {
     if (this.scrollY > 20) {
       $(".navbar").addClass("sticky");
@@ -10,7 +6,7 @@ $(document).ready(function () {
       $(".navbar").removeClass("sticky");
     }
   });
-  const home_content = document.querySelector(".home-content");
+
   const menu = document.querySelector(".navbarcontent");
 
   const headline = document.querySelector(".headline");
@@ -23,6 +19,7 @@ $(document).ready(function () {
     .fromTo(menu, 0.7, { opacity: 0 }, { opacity: 1 })
     .fromTo(headline, 0.7, { opacity: 0, x: 0 }, { opacity: 1, x: 0 }, "-=0.7")
     .fromTo(headline2, 0.7, { opacity: 0, x: 0 }, { opacity: 1, x: 0 }, "-=0.7")
+
     .fromTo(
       homebutton,
       0.7,
